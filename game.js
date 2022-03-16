20aae765f19b093c43e9fa2e1929905a52a8fc55
GabrielCART <gabriel.mascarenas@student.cart.org>
2022-03-16 10:29:32 -0700

    added story

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function stopSound(sound){ //audio stop playback function
 }
 const storyVars = {
     intro:{
-        text: `Your journey begins in the lovely city of New York. You come to your French class ready to apprendre, but your teacher; Le docteur Crowbar, has a better idea and decides that this year the entire class is taking a cruise to France. Of course everyone is ecstatic, even the class president, Billy Bobby Bones is cheerful. Soon enough, the big day arrives and everyone gets into the ship with smiling faces and happy hearts. But the Atlantic ocean wasn't so happy. That night you wake up to find yourself with a face full of sand and salty sea water. You realize that your ship has been wrecked and you're very close to an island with a lighthouse sitting atop it's cliffs. What do you want to do?`,
+        text: `Your journey begins in the lovely city of New York. You come to your French class ready to learn, but your teacher; Dr. Crowbar, has a better idea and decides that this year the entire class is taking a cruise to France. Of course everyone is ecstatic, even the class president, Billy Bobby Bones is cheerful. Soon enough, the big day arrives and everyone gets into the ship with smiling faces and happy hearts. But the Atlantic ocean wasn't so happy. That night you wake up to find yourself with a face full of sand and salty sea water. You realize that your ship has been wrecked and you're very close to an island with a lighthouse sitting atop it's cliffs. What do you want to do?`,
         options:[
             [`Try to swim to shore.`, `swimShore`],
             [`Let the currents take you.`, `notSwim`]
@@ -19,6 +19,38 @@ const storyVars = {
             [`Search for another way off the beach.`, `moveAroundCliffs`]
         ],
     },
+    moveAroundCliffs:{
+        text: `You start to look around, determined to get off the wretched island and go back home, you continue to walk around the base of the cliffs until you recognize something, or rather someone. It is Dr. Crowbar and the other survivors sitting on a beach where the cliffs drop low and the upper part of the island can be reached. Surprisingly, everyone is still alive and they are looking for supplies and rations, but everyone is torn between choosing a leader or picking berries. Do you want to let Dr. Crowbar lead or try to establish yourself as leader?`,
+        options:[
+            [`Let Dr. Crowbar lead.`, `crowbarLeads`],
+            [`Fight Dr. Crowbar for leadership.`, `fightCrowbar`],
+        ],
+    },
+    crowbarLeads:{
+        text: `You start to look around, determined to get off the wretched island and go back home, you continue to walk around the base of the cliffs until you recognize something, or rather someone. It is Dr. Crowbar and the other survivors sitting on a beach where the cliffs drop low and the upper part of the island can be reached. Surprisingly, everyone is still alive and they are looking for supplies and rations, but everyone is torn between choosing a leader or picking berries. Do you want to let Dr. Crowbar lead or try to establish yourself as leader?`,
+        options:[
+            [`Let Dr. Crowbar lead.`, `crowbarLeads`],
+            [`Fight Dr. Crowbar for leadership.`, `fightCrowbar`],
+        ],
+    },
+    fightCrowbar:{
+        text: `You decide to fight for leadership, taking down everyone, ahem, almost everyone, except Dr. Crowbar, who has a crowbar; obviously. You fight Dr. Crowbar with all strength, holding to your strong emotions and your non-existent oblivious strength, but Dr. Crowbar has the advantage; swinging his Crowbar like a madman, he strikes your weak point in your kneecaps. Do you give up or fight to defend your pride?`,
+        options:[
+            [`Give up.`, `fightCrowbarStop`],
+            [`Keep fighting!`, `fightCrowbarContinue`],
+        ],
+    },
+    fightCrowbarStop:{
+        text: `You plead with Dr. Crowbar to spare your life. Dr. Crowbar rethinks his decision and lets you live. He is impressed with your resilience and strength. Therefore, He makes you Second-in-Command. While scouting the area, The group happens upon a black labrador with a collar, But, you haven't eaten anything except Billy Bobby Bones'™ special dried, burnt fish with salty berry water stew; and you're getting tired of eating that abomination, so you want to settle on something less disgusting. You think about eating the dog, however, Dr. Crowbar can't decide whether he should follow the dog or not follow the dog.`,
+        options:[
+            [`Give up.`, `fightCrowbarStop`],
+            [`Keep fighting!`, `fightCrowbarContinue`],
+        ],
+    },
+    fightCrowbarContinue:{
+        text: `Apparently, Dr. Crowbar hates stubborn and proud children; he picks you up like a two-year-old child and heaves you over the cliff, you fall like a sack of potatoes and break all your bones on the rugged cliffs. <br> <br> Stubborn and Proud Child - Premature Ending`,
+        options:false,
+    },
     cliffClimb:{
         text: `The sharp rocks cut into your hands. The salty seawater stings your eyes, burns your cuts, and makes the rock slippery. The fatigue in your muscles builds and your arms begin to burn. You make it about halfway before your hands slip, failing to grasp the small crack in the rock just above you. As you fall you are forced to confront the fact that you won't survive this fall. You take it all in before falling into the waves. The last sound you ever hear is your bones being crushed against the rocky cliffs.<br> <br> Slippery Hands - Premature Ending`,
         options:false,
@@ -26,7 +58,7 @@ const storyVars = {
     notSwim:{
         text: `The currents take you around the island to another area off the coast. You think you can see the bright orange of life jackets littering the sand.`,
         options:[
-            [`Try to swim to shore now`, `swimShoreAround`],
+            [`Try to swim to shore now.`, `moveAroundCliffs`],
             [`Let the currents take you.`, `drown`]
         ],
     },
